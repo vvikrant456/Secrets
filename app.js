21ab8666748e460bcbfcb8e599fb08c541742ba1
VikasPc <vvikrant456@gmail.com>
2021-10-04 22:50:48 +0530

    level 5 to level 4

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const md5 = require("md5");
 const app = express();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 app.set('view engine', 'ejs');
 
@@ -15,7 +16,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-console.log(md5("123456"));
+
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true});
 
@@ -45,17 +46,23 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email:req.body.username,
-    password:md5(req.body.password)
-  });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets");
-    }
-  });
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+         // Store hash in your password DB.
+         const newUser = new User({
+           email:req.body.username,
+           password:hash
+         });
+         newUser.save(function(err){
+           if(err){
+             console.log(err);
+           }else{
+             res.render("secrets");
+           }
+         });
+     });
+
+
 });
 
 
@@ -67,9 +74,11 @@ User.findOne({email:username}, function(err, foundUser){
     console.log(err);
   }else{
     if(foundUser){
-      if(foundUser.password===password){
+bcrypt.compare(password, foundUser.password, function(err, result) {
+  if(result===true){
         res.render("secrets");
       }
+    });
     }
   }
 });
